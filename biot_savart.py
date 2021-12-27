c7a3d157a8a7b2f7cf1b72bc2eefd4f1b27ee3ad
bencarter4185 <bencarter4185@gmail.com>
2021-12-27 19:53:01 +0000

    Syncing unfinished biot-savart boilerplate code

---
+++ biot_savart.py
@@ -0,0 +1,18 @@
+"""
+Library file for Biot-Savart solver.
+"""
+
+import numpy as np
+
+class Wire:
+    """
+    Implements a wire of arbitrary shape and dimensions
+    """
+    
+    # Coordinates (x,y,z)
+    coordinates = []
+
+    # Functions to create wires
+
+
+
--- main.py
+++ main.py
@@ -1,34 +1,12 @@
 """
-Initial basic Python code for modelling the Biot-Savart law for a loop of wire
-
-At first I'm going to use external code as 'inspiration' before moving on to write my own implementation if appropriate.
-
-As it stands now this repository is simply a testbed for the Biot-Savart code found at https://github.com/vuthalab/biot-savart, with all rights going to Mingde Yin and Ryan Zazo
+Writing a biot-savart solver from scratch
 """
 
-"""
-This is the `Helmholtz Coils` example as shown in https://github.com/vuthalab/biot-savart#helmholtz-coils
-"""
+def main():
 
-import biot_savart_v4_3 as bs
+    i = complex(1, 0)
 
-bs.helmholtz_coils("helm1.txt", "helm2.txt", 50, 5, 2, 1)
-# makes a pair of helmholtz coils
-# 50 segments each, with radius of 5 cm
-# spaced out by 2 cm, located at z = +/- 1 respectively
-# 1 amp of current
+    print(f"i = {i}")
 
-bs.plot_coil("helm1.txt", "helm2.txt")
-
-bs.write_target_volume("helm1.txt", "targetvol1", (10, 10, 10), (-5, -5, -5), 0.5, 0.5)
-bs.write_target_volume("helm2.txt", "targetvol2", (10, 10, 10), (-5, -5, -5), 0.5, 0.5)
-# use a target volume of size 10, centred about origin
-
-h1 = bs.read_target_volume("targetvol1")
-h2 = bs.read_target_volume("targetvol2")
-# produce the target volumes we want
-
-# use linear superposition of magnetic fields, to get the combined effects of multiple coils
-h_total = h1 + h2
-
-bs.plot_fields(h_total, (10, 10, 10), (-5, -5, -5), 0.5, which_plane='z', level=0, num_contours=50)
+if __name__ == '__main__':
+    main()
